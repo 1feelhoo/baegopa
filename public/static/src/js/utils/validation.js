66b62568617c5568072e37c4f239f9df5648de09
1feelhoo <hanfeelhoo@LAPTOP-MS4GG21Q.localdomain>
2022-03-25 23:03:42 +0900

    테스트

--- public/static/src/js/utils/validation.js
+++ public/static/src/js/utils/validation.js
@@ -1,14 +1,15 @@
 import { authService } from "./firebase.js";
 
 const isLogin = () => {
+    console.log("check login...");
     authService.onAuthStateChanged((user) => {
         if(user){
             document.querySelector(".login").style.display = "none";
             document.querySelector(".logout").style.display = "block";
         }
         else {
-            document.querySelector(".login").style.display = "block";
             document.querySelector(".logout").style.display = "none";
+            document.querySelector(".login").style.display = "block";
             if(location.pathname !== "/login") {
                 window.location.href = "/login";
             }
